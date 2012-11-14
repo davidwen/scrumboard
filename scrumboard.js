de2837b27e1971d7c7986d33b453603bb903a8f6
David Wen <david@dwen.me>
2012-11-14 01:04:51 -0500

    Edit task

--- scrumboard.js
+++ scrumboard.js
@@ -10,6 +10,9 @@ var getSprint = function(name) {
 }
 
 var getStory = function(sprint, storyName) {
+  if (!sprint) {
+    return null;
+  }
   for (var ii = 0; ii < sprint.stories.length; ii++) {
     var story = sprint.stories[ii];
     if (story.name == storyName) {
@@ -20,6 +23,9 @@ var getStory = function(sprint, storyName) {
 }
 
 var getTask = function(story, taskId) {
+  if (!story) {
+    return null;
+  }
   for (var jj = 0; jj < story.tasks.length; jj++) {
     var task = story.tasks[jj];
     if (task.id == taskId) {
@@ -73,9 +79,13 @@ if (Meteor.isClient) {
     },
 
     'click .show-add-task-dialog': function() {
-      $('form.add-task-form')[0].reset();
-      $('#add-task-dialog').find('.add-task').text('Add Task To ' + this.name);
-      $('form.add-task-form').find('#story-name').val(this.name);
+      var $form = $('form.add-task-form');
+      var $dialog = $('#add-task-dialog');
+      $form[0].reset();
+      $form.find('#story-name').val(this.name);
+      $dialog.find('.add-task').text('Add Task To ' + this.name);
+      $dialog.find('.task-header').hide();
+      $dialog.find('.new-task-header').show();
     }
   }
 
@@ -116,26 +126,32 @@ if (Meteor.isClient) {
       var taskDescription = $form.find('#task-description').val();
       var taskStatus = $form.find('#task-status').val();
       var storyName = $form.find('#story-name').val();
+      var taskId = $form.find('#task-id').val();
       if (taskName && taskOwner && taskHours && taskDescription && taskStatus) {
-        var newTask = {
-          name: taskName,
-          owner: taskOwner,
-          hours: taskHours,
-          description: taskDescription,
-          status: taskStatus
-        };
-        var newStories = sprint.stories;
-        var numStories = newStories.length;
-        for (var ii = 0; ii < numStories; ii++) {
-          var story = newStories[ii];
-          if (story.name == storyName) {
+        if (taskId) {
+          var story = getStory(sprint, storyName);
+          var task = getTask(story, taskId);
+          task.name = taskName;
+          task.owner = taskOwner;
+          task.hours = taskHours;
+          task.description = taskDescription;
+          task.status = taskStatus;
+        } else {
+          var newTask = {
+            name: taskName,
+            owner: taskOwner,
+            hours: taskHours,
+            description: taskDescription,
+            status: taskStatus
+          };
+          var story = getStory(sprint, storyName);
+          if (story) {
             newTask.id = story.nextTaskId;
             story.nextTaskId++;
             story.tasks.push(newTask);
-            break;
           }
         }
-        Sprints.update({_id: sprint._id}, {$set: {stories: newStories}});
+        Sprints.update({_id: sprint._id}, {$set: {stories: sprint.stories}});
         $('#add-task-dialog').modal('hide');
       } else {
         alert('All fields required');
@@ -158,10 +174,12 @@ if (Meteor.isClient) {
 
     var $tr = $(this.find('tr'));
     $(this.findAll('.task')).draggable({
+      cancel: '.edit-task',
       containment: $tr[0],
       cursor: 'move',
       revert: 'invalid',
-      revertDuration: 80
+      revertDuration: 80,
+      zIndex: 100
     });
 
     $(this.findAll('td:not(.story-cell)')).droppable({
@@ -172,15 +190,44 @@ if (Meteor.isClient) {
         var sprint = getSprint();
         var storyName = $(this).closest('tr').find('.story-name').text();
         var story = getStory(sprint, storyName);
-        if (story) {
-          var task = getTask(story, taskId);
-          if (task && task.status != newStatus) {
-            task.status = newStatus;
-            Sprints.update({_id: sprint._id}, {$set: {stories: sprint.stories}});
-          }
+        var task = getTask(story, taskId);
+        if (task && task.status != newStatus) {
+          task.status = newStatus;
+          Sprints.update({_id: sprint._id}, {$set: {stories: sprint.stories}});
         }
       },
       hoverClass: 'task-hover'
+    });
+
+    $tr.on('mouseenter', '.task', function() {
+      $(this).find('.edit-task').css('visibility', 'visible');
+    }).on('mouseleave', '.task', function() {
+      $(this).find('.edit-task').css('visibility', 'hidden');
+    });
+
+    $('.edit-task').click(function() {
+      var storyName = $(this).closest('tr').find('.story-name').text();
+      var taskId = $(this).closest('.task').find('input[name=taskId]').val();
+
+      var sprint = getSprint();
+      var story = getStory(sprint, storyName);
+      var task = getTask(story, taskId);
+      if (task) {
+        var $form = $('form.add-task-form');
+        var $dialog = $('#add-task-dialog');
+        $form[0].reset();
+        $form.find('#story-name').val(storyName);
+        $form.find('#task-id').val(taskId);
+        $form.find('#task-name').val(task.name);
+        $form.find('#task-owner').val(task.owner);
+        $form.find('#task-hours').val(task.hours);
+        $form.find('#task-description').val(task.description);
+        $form.find('#task-status').val(task.status);
+        $dialog.find('.add-task').text('Save');
+        $dialog.find('.task-header').hide();
+        $dialog.find('.edit-task-header').show();
+        $dialog.modal({});
+      }
     });
   }
 }
