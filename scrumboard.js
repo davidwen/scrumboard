e52852e24db25008bf25ebf21e1fe488b7a01c3f
David Wen <david@dwen.me>
2012-11-14 00:24:55 -0500

    Drag and drop

--- scrumboard.js
+++ scrumboard.js
@@ -8,6 +8,29 @@ var getSprint = function(name) {
   }
   return Sprints.findOne({ name: name });
 }
+window.getSprint = getSprint;
+
+var getStory = function(sprint, storyName) {
+  for (var ii = 0; ii < sprint.stories.length; ii++) {
+    var story = sprint.stories[ii];
+    if (story.name == storyName) {
+      return story;
+    }
+  }
+  return null;
+}
+window.getStory = getStory;
+
+var getTask = function(story, taskId) {
+  for (var jj = 0; jj < story.tasks.length; jj++) {
+    var task = story.tasks[jj];
+    if (task.id == taskId) {
+      return task;
+    }
+  }
+  return null;
+}
+window.getTask = getTask;
 
 var parseImport = function(input) {
   var rows = [];
@@ -29,6 +52,74 @@ var parseImport = function(input) {
   }
   rows.push(currentRow);
   return rows;
+}
+
+var submitAddSprintForm = function() {
+  var $form = $('form.add-sprint-form');
+  var sprintName = $form.find('#sprint-name').val();
+  if (sprintName) {
+    if (getSprint(sprintName)) {
+      alert('Sprint with that name already exists');
+    } else {
+      var stories = [];
+      var storiesImport = $form.find('#sprint-stories-import').val();
+      if (storiesImport) {
+        var storyRows = parseImport(storiesImport);
+        for (var ii = 0; ii < storyRows.length; ii++) {
+          var storyRow = storyRows[ii];
+          var story = {
+            name: storyRow[0],
+            description: storyRow[2],
+            acceptanceCriteria: storyRow[3],
+            points: Number(storyRow[4]),
+            tasks: [],
+            nextTaskId: 0
+          };
+          stories.push(story);
+        }
+
+        var tasksImport = $form.find('#sprint-tasks-import').val();
+        if (tasksImport) {
+          var taskRows = parseImport(tasksImport);
+          var currentStory;
+          for (var ii = 0; ii < taskRows.length; ii++) {
+            var taskRow = taskRows[ii];
+            var task = {
+              name: taskRow[1],
+              description: taskRow[2],
+              owner: taskRow[4],
+              hours: Number(taskRow[5]),
+              status: 'notstarted'
+            }
+            var storyName = taskRow[0]
+            if (storyName) {
+              currentStory = storyName;
+            }
+            for (var jj = 0; jj < stories.length; jj++) {
+              var story = stories[jj];
+              if (story.name == currentStory) {
+                task.id = story.nextTaskId;
+                story.nextTaskId++;
+                story.tasks.push(task);
+                break;
+              }
+            }
+          }
+        }
+      }
+
+      Sprints.insert(
+        {name: sprintName, stories: stories},
+        function(error, result) {
+          if (result) {
+            window.location = '/' + sprintName;
+          }
+        });
+      $('#import-sprint-dialog').modal('hide');
+    }
+  } else {
+    alert('Name required');
+  }
 }
 
 if (Meteor.isClient) {
@@ -63,76 +154,12 @@ if (Meteor.isClient) {
 
   Template.addSprintDialog.events = {
     'submit .add-sprint-form': function() {
-      var $form = $('form.add-sprint-form');
-      var sprintName = $form.find('#sprint-name').val();
-      if (sprintName) {
-        if (getSprint(sprintName)) {
-          alert('Sprint with that name already exists');
-        } else {
-          var stories = [];
-          var storiesImport = $form.find('#sprint-stories-import').val();
-          if (storiesImport) {
-            var storyRows = parseImport(storiesImport);
-            for (var ii = 0; ii < storyRows.length; ii++) {
-              var storyRow = storyRows[ii];
-              var story = {
-                name: storyRow[0],
-                description: storyRow[2],
-                acceptanceCriteria: storyRow[3],
-                points: Number(storyRow[4]),
-                tasks: [],
-                nextTaskId: 0
-              };
-              stories.push(story);
-            }
-
-            var tasksImport = $form.find('#sprint-tasks-import').val();
-            if (tasksImport) {
-              var taskRows = parseImport(tasksImport);
-              var currentStory;
-              for (var ii = 0; ii < taskRows.length; ii++) {
-                var taskRow = taskRows[ii];
-                var task = {
-                  name: taskRow[1],
-                  description: taskRow[2],
-                  owner: taskRow[4],
-                  hours: Number(taskRow[5]),
-                  status: 'notstarted'
-                }
-                var storyName = taskRow[0]
-                if (storyName) {
-                  currentStory = storyName;
-                }
-                for (var jj = 0; jj < stories.length; jj++) {
-                  var story = stories[jj];
-                  if (story.name == currentStory) {
-                    task.id = story.nextTaskId;
-                    story.nextTaskId++;
-                    story.tasks.push(task);
-                    break;
-                  }
-                }
-              }
-            }
-          }
-
-          Sprints.insert(
-            {name: sprintName, stories: stories},
-            function(error, result) {
-              if (result) {
-                window.location = '/' + sprintName;
-              }
-            });
-          $('#import-sprint-dialog').modal('hide');
-        }
-      } else {
-        alert('Name required');
-      }
+      submitAddSprintForm();
       return false;
     },
 
     'click .add-sprint': function() {
-      $('form.add-sprint-form').submit();
+      submitAddSprintForm(); 
     }
   }
 
@@ -233,8 +260,39 @@ if (Meteor.isClient) {
   }
 
   Template.story.rendered = function() {
-    $(this.find('.tasks-container')).masonry({
-      itemSelector : '.task',
+    var $container = $(this.findAll('.tasks-container'));
+    $(window).resize(function() {
+      $container.masonry({
+        itemSelector : '.task',
+      });
+    });
+    $(window).trigger('resize');
+
+    var $tr = $(this.find('tr'));
+    $(this.findAll('.task')).draggable({
+      containment: $tr[0],
+      cursor: 'move',
+      revert: 'invalid',
+      revertDuration: 80
+    });
+
+    $(this.findAll('td:not(.story-cell)')).droppable({
+      drop: function(event, ui) {
+        var taskId = $(ui.draggable).find('input[name=taskId]').val();
+        var newStatus = $(this).find('input[name=status]').val();
+
+        var sprint = getSprint();
+        var storyName = $(this).closest('tr').find('.story-name').text();
+        var story = getStory(sprint, storyName);
+        if (story) {
+          var task = getTask(story, taskId);
+          if (task && task.status != newStatus) {
+            task.status = newStatus;
+            Sprints.update({_id: sprint._id}, {$set: {stories: sprint.stories}});
+          }
+        }
+      },
+      hoverClass: 'task-hover'
     });
   }
 }
