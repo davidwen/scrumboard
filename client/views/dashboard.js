e64e5b167ad8f864a9291ff19c1bfa8244b71325
David Wen <david@dwen.me>
2013-01-03 22:59:33 -0500

    Edit/delete sprint

--- client/views/dashboard.js
+++ client/views/dashboard.js
@@ -17,5 +17,21 @@ Template.dashboardSprint.stories = function() {
 Template.dashboard.events = {
   'click #show-add-sprint-dialog': function() {
     $('#add-sprint-form')[0].reset();
+  },
+
+  'mouseenter .sprint-row': function() {
+    $(event.target).find('.edit-sprint').show();
+  },
+
+  'mouseleave .sprint-row': function() {
+    $(event.target).find('.edit-sprint').hide();
+  },
+
+  'click .edit-sprint': function() {
+    var sprint = getSprint($(event.target).closest('tr').attr('data-sprint-id'));
+    var $dialog = $('#edit-sprint-dialog');
+    $dialog.attr('data-sprint-id', sprint._id);
+    $dialog.find('#sprint-name').val(sprint.name);
+    $dialog.find('.error').empty().hide();
   }
 }
